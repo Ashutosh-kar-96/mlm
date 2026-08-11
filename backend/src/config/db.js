4a6e2949ced1e335d780f3ce5028ff925de6da0b
Ashutosh-kar-96 <ashutoshkar96922@gmail.com>
2026-08-11 15:36:29 +0530

    refresh backend dependencies

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,5 +1,28 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
+
+// export default prisma;
+
+
+
+
+import { PrismaClient } from "@prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+const databaseUrl = new URL(process.env.DATABASE_URL);
+
+const adapter = new PrismaMariaDb({
+  host: databaseUrl.hostname,
+  port: Number(databaseUrl.port || 3306),
+  user: decodeURIComponent(databaseUrl.username),
+  password: decodeURIComponent(databaseUrl.password),
+  database: databaseUrl.pathname.replace(/^\//, ""),
+  connectionLimit: 5,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
 
 export default prisma;
